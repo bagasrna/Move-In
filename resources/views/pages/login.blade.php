2544dfe9cc71cc5ebefe794a16d00275724951df
Natabagas <meganatabagas@gmail.com>
2022-12-06 01:53:01 +0700

    Membuat page login dll

---
+++ resources/views/pages/login.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Move-In</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <div class="flex flex-row">
+        <img class="h-screen" src="/image/delivery.png" alt="">
+        <form action="/login" method="POST">
+            @csrf
+            <div class="flex px-[80px] pt-[200px] flex-col">
+                <h1 class="font-bold text-[24px]">Login</h1>
+                <div class="flex flex-row">
+                    <div class="flex flex-col mt-7 font-Open Sans">
+                        <input type="text" name="full_name" class="w-[500px] border rounded-xl bg-[#D6E4E5] mt-5 p-3 placeholder:-pl-[600px] placeholder:text-[black]" placeholder="Full Name">
+                        <input type="password" name="password" class="w-[500px] border rounded-xl bg-[#D6E4E5] mt-7 p-3 placeholder:-pl-[600px] placeholder:text-[black]" placeholder="Password">
+                    </div>
+                </div>
+                <button type="submit" class="bg-[#4FA095] mt-[50px] mb-3 text-white p-3 rounded-xl ">LOGIN</button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        "Open Sans": "Open Sans"
+      }
+    },
   },
   plugins: [],
 }
