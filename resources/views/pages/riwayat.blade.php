e4726fc53e2a27d03bbe658a08b83e86feafffe5
Natabagas <meganatabagas@gmail.com>
2022-12-06 11:22:53 +0700

    PUSH DONE

--- resources/views/pages/riwayat.blade.php
+++ resources/views/pages/riwayat.blade.php
@@ -12,19 +12,16 @@
         <h1 class="mt-[120px] text-[24px] font-bold">Pesanan</h1>
     </div>
     <div class="flex flex-row mt-[50px] justify-around p-5 bg-[#D6E4E5]">
-        <h1>Sedang berjalan</h1>
-        <h1>Selesai</h1>
+        <h1 class="font-bold text-[20px]">Selesai</h1>
     </div>
-    <div class="bg-[#2A3342]">
-        <div class="w-full cursor-pointer px-[140px] flex-row">
         @foreach ($histories as $history)
-            <a href="/book/{{$book->id}}" class="w-full p-5 mt-[20px] mb-[50px]">
-                <div class="px-7 h-[400px] py-[50px] flex font-semibold text-[25px] flex-col rounded-2xl bg-[#D9D9D9]">
-                    <h1 class="text-[#2a33429c] w-[500px]">{{$histry->date}}</h1>
-                    <h3 class="text-[#2a33429c] mt-[50px] w-[300px]">By : <span class="underline">{{$history->quantity}}</span></h3>
+            <div" class="flex flex-row w-[600px]">
+                <div class="px-7 h-[350px] text-white p-5 items-center mx-5 mt-[50px] w-full justify-center flex font-semibold text-[20px] flex-col rounded-2xl bg-[#F6F6C9]">
+                    <h1 class="w-[300px]  bg-[#9F9F9F] rounded-2xl py-2 px-5">Tanggal pesan : {{$history->date}}</h1>
+                    <h3 class="mt-[50px] bg-[#9F9F9F] rounded-2xl py-2 px-5 w-[300px]">Quantitiy : {{$history->quantity}}</h3>
+                    <h3 class="mt-[50px] bg-[#9F9F9F] rounded-2xl px-5 py-2 w-[300px]">Total : {{$history->total}}</h3>
                 </div>
             </a>
         @endforeach
-    </div>
 </body>
 </html>
